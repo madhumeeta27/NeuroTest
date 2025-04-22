c0cf905ba5af70f178f462da7494f768a09ae767
nithya1365 <nithyajayakumar13@gmail.com>
2025-04-22 16:05:32 +0530

    first commit

---
+++ bci/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import Hello from './components/Hello'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        welcomeeeeee
+        <Hello/>
+
+        
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ bci/src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+        hello this is from the other file
+      
+    </div>
+  )
+}
+
+export default Hello
